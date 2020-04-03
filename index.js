eaefd7fc8866b8b36f2fc0b0e81f6521df943cc7
Will Brown <wtbrown10@gmail.com>
2020-04-02 20:20:48 -0400

    added console log on line 6

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-let currentDayOfWeek = Monday;
-let currentMonth = April;
+let currentDayOfWeek = "Monday";
+let currentMonth = "April";
 let currentDay = 2;
+let currentYear = 2020;
+
+console.log(
+  "Today is:",
+  currentDayOfWeek,
+  currentMonth,
+  currentDay,
+  currentYear
+);
