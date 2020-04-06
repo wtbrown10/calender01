05c98ed6047c2a608a12e98c7e754b0d42f5b462
Will Brown <wtbrown10@gmail.com>
2020-04-05 20:46:16 -0400

    added gitpractice.js

---
+++ gitpractice.js
@@ -0,0 +1,12 @@
+let currentDayOfWeek = "Monday";
+let currentMonth = "April";
+let currentDay = 2;
+let currentYear = 2020;
+
+console.log(
+  "Today is:",
+  currentDayOfWeek,
+  currentMonth,
+  currentDay,
+  currentYear
+);
